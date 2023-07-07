f78778f66907193c95ee9281ce40ff6ae81807e2
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-07-07 21:32:51 +0100

    RootLayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,11 @@
 import { Fragment } from "react"
 import { Outlet } from "react-router-dom"
+import NavBar from "../components/NavBar"
 
 export default function RootLayout() {
   return (
     <Fragment>
+      <NavBar />
       <Outlet />
     </Fragment>
   )
