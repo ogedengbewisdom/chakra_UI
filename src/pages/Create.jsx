df3d52d07fdc1d93423c19e6bcfac9714b67c99c
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-07-08 16:29:13 +0100

    Tabs

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,37 @@
+import { Tab, TabList, TabPanel, TabPanels, Tabs, SimpleGrid, Box, Heading} from "@chakra-ui/react";
+import Dashboard from "./Dashboard";
+
 export default function Create() {
   return (
-    <div>Create</div>
+    <Tabs mt={"40px"} p={"20px"} colorScheme="red" >
+      <TabList>
+        <Tab >One</Tab>
+        <Tab _selected={{color: "blue", bg: "purple"}}>Two</Tab>
+        <Tab _selected={{color: "blue", bg: "purple"}} borderRadius={"10px"}>Three</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>One!</TabPanel>
+        <TabPanel>
+          <Heading>Dashboard</Heading>
+        <SimpleGrid spacing={10} minChildWidth={"300px"}>
+      <Box height={"200px"} border={"1px solid"}></Box>
+      <Box height={"200px"} border={"1px solid"}></Box>
+      <Box height={"200px"} border={"1px solid"}></Box>
+      <Box height={"200px"} border={"1px solid"}></Box>
+
+      <Box height={"200px"} border={"1px solid"}></Box>
+      <Box height={"200px"} border={"1px solid"}></Box>
+      <Box height={"200px"} border={"1px solid"}></Box>
+      <Box height={"200px"} border={"1px solid"}></Box>
+
+      <Box height={"200px"} border={"1px solid"}></Box>
+      <Box height={"200px"} border={"1px solid"}></Box>
+      <Box height={"200px"} border={"1px solid"}></Box>
+      <Box height={"200px"} border={"1px solid"}></Box>
+    </SimpleGrid>
+        </TabPanel>
+        <TabPanel>Three!</TabPanel>
+      </TabPanels>
+    </Tabs>
   )
 }
