7cfc831b20f199a31d9a83847323ffde924c7118
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-07-09 16:05:10 +0100

    profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,68 @@
+import { ChatIcon, CheckCircleIcon, EmailIcon, StarIcon } from "@chakra-ui/icons";
+import { TabList, Tabs, Tab, TabPanels, TabPanel, ListItem, ListIcon, List } from "@chakra-ui/react";
+
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <Tabs variant={"enclosed"} mt={"40px"} padding={"20px"}>
+      <TabList>
+        <Tab _selected={{bg: "purple.400", color: "white"}} borderTop={"15px"}>Account Info</Tab>
+        <Tab _selected={{bg: "purple.400", color: "white"}}>Task History</Tab>
+      </TabList>
+
+      <TabPanels>
+        <TabPanel>
+          <List>
+            <ListItem mt={"10px"}>
+              <ListIcon as={EmailIcon} />
+              Email: wisdom@gmail.com
+            </ListItem>
+
+            <ListItem mt={"10px"}>
+              <ListIcon as={ChatIcon} />
+              Lorem ipsum dolor, sit amet consectetur adipisicing.
+            </ListItem>
+
+            <ListItem mt={"10px"}>
+              <ListIcon as={StarIcon} />
+              Start boy
+            </ListItem>
+          </List>
+        </TabPanel>
+
+        <TabPanel>
+          <List>
+            <ListItem mt={"10px"}>
+              <ListIcon as={CheckCircleIcon} />
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, aspernatur.
+            </ListItem>
+
+            <ListItem mt={"10px"}>
+              <ListIcon as={CheckCircleIcon} />
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, aspernatur.
+            </ListItem>
+
+            <ListItem mt={"10px"}>
+              <ListIcon as={CheckCircleIcon} color={"teal.400"} />
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, aspernatur.
+            </ListItem>
+
+            <ListItem mt={"10px"}>
+              <ListIcon as={CheckCircleIcon} color={"teal.400"} />
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, aspernatur.
+            </ListItem>
+
+            <ListItem mt={"10px"}>
+              <ListIcon as={CheckCircleIcon} color={"teal.400"} />
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, aspernatur.
+            </ListItem>
+
+            <ListItem mt={"10px"}>
+              <ListIcon as={CheckCircleIcon} />
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, aspernatur.
+            </ListItem>
+          </List>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   )
 }
